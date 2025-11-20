28071db9a3e6ffbbcd0d773e572e213a64168b94
Arrvinna Dewi Mogan <arrvinna@gmail.com>
2025-11-20 22:15:25 +0800

    Fix Node version

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,4 +8,10 @@ const config = {
   }
 };
 
-export default config;
+export default {
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs20.x'
+    })
+  }
+};
